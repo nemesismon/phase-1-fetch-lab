f8bc3db7b335a2c0b9bbec10b2610f804cc35244
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-18 14:54:35 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+ return fetch('https://anapioficeandfire.com/api/books')
   .then((res) => res.json())
   .then((data) => renderBooks(data))
 }
@@ -16,5 +16,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  const x = fetchBooks();
+  console.log(x);
 });
